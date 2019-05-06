93948526072c513db383c86c1f43fb0a38854ef9
leve1986 <jerome.leve@neuf.fr>
2019-05-06 15:07:43 +0200

    validation test

---
+++ java-web-project/src/Test/webapp/test.java
@@ -0,0 +1,14 @@
+package main;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class Test {
+
+	public static void main(String[] args) {
+	    assertEquals(1, 2);
+	}
+    
+    }
+
